0218c3465210f4ac994d52d702539cbbdcc72a16
CHARLAT Jerome <jerome.charlat@orange.fr>
2019-07-19 17:41:54 +0200

    Add route

--- routes/user/middleware.js
+++ routes/user/middleware.js
@@ -11,13 +11,13 @@ router.use(function(req, res, next){
 
     if(token){
         jwt.verify(token, secretWord, function(err, decoded){
-            if(decoded.xsrfToken != xsrfToken){
+            /*if(decoded.xsrfToken != xsrfToken){
                 console.log("!!!!! ATTAQUE TOKEN XSRF !!!!!")
                 return res.json({ success: false, message: 'Attaque token XSRF'})
             }
             else if(err){
                 return res.json({ success: false, message: 'Failed to authenticate token'});
-            }
+            }*/
             req.decoded = decoded;
             next();
         })
--- routes/user/user.js
+++ routes/user/user.js
@@ -1,6 +1,25 @@
 var express = require('express')
 var router = express.Router()
 const User = require('../../models/User')
+const Country = require('../../models/Country')
+let mongoose = require('mongoose')
+
+router.get('/users_info', function(req, res, next){
+    User.find({}, {name: 1, firstName: 1, _id: 0})
+    .then(user =>{
+        if(user){
+            res.json(user);
+        }
+        else
+        {
+            res.json({
+                success: false,
+                result: false,
+                message: "Error " + err
+            })
+        }
+    })
+})
 
 router.get('/user/:id', function(req, res, next){
     User.findById(req.params.id).populate('countries')
@@ -104,6 +123,46 @@ router.put('/thisuser', function(req, res, next){
                     message: "User updated"
                 })
             })
+            .catch(err => {
+                res.status(400).send({
+                    success: false,
+                    message: "Error: " + err
+                })
+            })
+    }
+    else
+    {
+        res.status(400).send({
+            success: false,
+            message: "Une erreur est survenue"
+        })
+    }
+})
+
+router.put('/thisuser_addcountry', function(req, res, next){
+    if(!req.body.alpha3){
+        res.json({
+            success: false,
+            message: req.body
+        })
+    }
+    else if(req.decoded.id){
+        Country.findOne({ alpha3: req.body.alpha3 }, {_id : 1})
+            .then(country => {
+                User.updateOne({ _id: req.decoded.id }, { $push: { countries: mongoose.Types.ObjectId(country._id)}})
+                    .then(() => {
+                        res.json({
+                            success: true,
+                            message: "User updated"
+                        })
+                    })
+                    .catch(err => {
+                        res.status(400).send({
+                            success:false,
+                            message: "Error " + err
+                        })
+                    })
+                })       
             .catch(err => {
                 res.status(400).send({
                     success: false,
